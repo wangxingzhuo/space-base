0a32e99ea2cc09fdc9a1bd6e266f2fc499351dc0
James <hamishwatson@live.com>
2026-05-16 22:53:45 +0800

    update

--- @watsonserve/utils/rollup.config.js
+++ @watsonserve/utils/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 import * as path from 'path'
 import * as fsp from 'fs/promises'
 
